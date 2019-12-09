9a8d48b236c9e928aadc52a2a46a43ea2d72d54d
Xiao Yu <xiao.susan.yu@gmail.com>
2019-12-09 14:43:34 +0800

    [Xiao]{feat}: add printReceipt function

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,26 +1,14 @@
-var selectedItems = ["ITEM0013 x 4", "ITEM0022 x 1"];
-var result = bestCharge(selectedItems);
-console.log(listItemDetails(selectedItems));
-console.log(result);
-function bestCharge(selectedItems) {
-  var itemArr = listItemDetails(selectedItems);
-  var charge = calculateCharge(itemArr);
-  var promotionPrice = calculateChargeAfterPromotion(charge, itemArr);
-  return promotionPrice.reduce(function(prev, element) {
-    return (prev <= element) ? prev : element;
-  });
-}
-//用户输入的数据["ITEM0013 x 4", "ITEM0022 x 1"],在菜品价目表中找出价格等，每个item以object形式存放, 加入count，结果存为数组
-//菜品价目列表--loadAllItems()
-//优惠列表--loadPromotions()
-//计算输入item的价格charge数字：calculateCharge，输入是array，输出是number
-//计算优惠：各种优惠价格都算出来放在一个数组promotionPrice，输入有number的总价，array的菜品itemArr(itemArr[i]是object);
-//在promotionPrice中比较优惠价格大小，输出最小的价格--主函数
+var selectedItems = ["ITEM0013 x 4"];
+var itemArr = listItemDetails(selectedItems);
+var discountedPrice = bestCharge(itemArr);
+var receipt = printReceipt(itemArr, discountedPrice);
+console.log(receipt);
+
 function listItemDetails(array) {
-  return array.map(function(element) {
+  return array.map(function (element) {
     var splited = element.split(" x ");
     var itemList = loadAllItems();
-    return itemList.reduce(function(prev, curr) {
+    return itemList.reduce(function (prev, curr) {
       if (curr.id === splited[0]) {
         curr.count = Number(splited[1]);
         return prev = curr;
@@ -29,20 +17,22 @@ function listItemDetails(array) {
     }, {});
   })
 }
+
 function calculateCharge(array) {
-  return array.reduce(function(prev, curr) {
+  return array.reduce(function (prev, curr) {
     return prev += curr.price * curr.count;
   }, 0);
 }
+
 function calculateChargeAfterPromotion(num, array) {
   var itemObj = {
     charge: 0,
     items: ['ITEM0001', 'ITEM0022'],
-    promotion1: function() {
+    promotion1: function () {
       return this.charge - ~~(this.charge / 30) * 6;
     },
-    promotion2: function(array) {
-      return array.reduce(function(prev, curr) {
+    promotion2: function (array) {
+      return array.reduce(function (prev, curr) {
         var currCharge = curr.price * curr.count;
         if (itemObj.items.includes(curr.id)) {
           currCharge = currCharge / 2;
@@ -56,6 +46,40 @@ function calculateChargeAfterPromotion(num, array) {
   return chargeArr.concat(itemObj.promotion1(), itemObj.promotion2(array));
 }
 
+function bestCharge(itemArr) {
+  var charge = calculateCharge(itemArr);
+  var promotionPrice = calculateChargeAfterPromotion(charge, itemArr);
+  var minPrice = charge;
+  var minPromotionType = "";
+  var saveMoney = "";
+  promotionPrice.forEach(function (element, index) {
+    if (element < minPrice) {
+      minPrice = element;
+      minPromotionType = loadPromotions()[index].type;
+      saveMoney = charge - minPrice;
+    }
+  });
+  var result = {};
+  result.price = minPrice;
+  result.type = minPromotionType;
+  result.save = saveMoney;
+  return result;
+}
+
+function printReceipt(itemArray, priceObj) {
+  var receipt = "============= 订餐明细 =============\n";
+  for (var i = 0; i < itemArray.length; i++) {
+    receipt += itemArray[i].name + " x " + itemArray[i].count + " = " + itemArray[i].price * itemArray[i].count + "元\n";
+  }
+  receipt += "-----------------------------------\n"
+  if (priceObj.type) {
+    receipt += "使用优惠：\n" + priceObj.type
+      + "，省" + priceObj.save + "元\n"
+      + "-----------------------------------\n";
+  }
+  receipt += "总计：" + priceObj.price + "元\n" + "===================================";
+  return receipt;
+}
 
 function loadAllItems() {
   return [{
@@ -76,11 +100,31 @@ function loadAllItems() {
     price: 2.00
   }];
 }
+
 function loadPromotions() {
-  return [{
+  var promotionList = [{
     type: '满30减6元'
   }, {
     type: '指定菜品半价',
     items: ['ITEM0001', 'ITEM0022']
   }];
+  var discountedItemId = promotionList[1].items;
+  var discountedItemName = discountedItemId.map(function (element) {
+    for (var i = 0; i < loadAllItems().length; i++) {
+      if (loadAllItems()[i].id === element) {
+        element = loadAllItems()[i].name;
+      }
+    }
+    return element;
+  })
+  promotionList[1].type += "(" + discountedItemName + ")";
+  return promotionList;
 }
+
+//[done]用户输入的数据,在菜品价目表中找出价格等，每个item以object形式存放, 加入count，结果存为数组
+//[provided]菜品价目列表--loadAllItems()
+//[provided]优惠列表--loadPromotions()
+//[done]计算输入item的价格charge数字：calculateCharge，输入是array，输出是number
+//[done]计算优惠：各种优惠价格都算出来放在一个数组promotionPrice，输入有number的总价，array的菜品itemArr(itemArr[i]是object);
+//[done]在promotionPrice中比较优惠价格大小，输出最小的价格以及优惠类型的object--主函数bestCharge
+//[to-do]最后按照格式输出清单
