7a8965609f451272856ef7726348578a9691a45b
Xiao Yu <xiao.susan.yu@gmail.com>
2019-12-10 17:32:06 +0800

    [Xiao]{comment}: add comment to practice tasking

--- src/best-charge.js
+++ src/best-charge.js
@@ -125,10 +125,63 @@ function loadPromotions() {
 }
 
 //[done]用户输入的数据,在菜品价目表中找出价格等，每个item以object形式存放, 加入count，结果存为数组
+  /*输入：
+    selectedItems: [string]
+    itemlist: [{
+      id: string,
+      name: string,
+      price: number
+    }, ...]
+    输出：
+    itemArr: [{
+      id: string,
+      name: string,
+      price: string,
+      count: number,
+    }, ...]
+  */
 //[provided]菜品价目列表--loadAllItems()，无需argument，输出数组
 //[done]优惠列表--loadPromotions(),type里面的内容根据id列出具体打折的商品，无需argument，输出数组
+  /* 输入：
+    hard code promotionlist: [{
+      type: string,
+      items: string,
+    }]
+    输出：
+    promotionlist: [{
+      type: string (add promoted item details),
+      items: string,
+    }]
+   */
 //[done]计算输入item的价格charge数字：calculateCharge，输入是array，输出是number
+  /*输入：
+    itemArr
+  输出：
+    charge: number
+  */
 //[done]计算优惠：各种优惠价格都算出来放在一个数组promotionPrice，输入有number的总价，array的菜品itemArr(itemArr[i]是object);输出的是包含各种优惠价格的数组；
+  /*输入：
+    charge
+    itemArr
+  输出:
+    promotionPrice: [number]
+  */
 //[done]在promotionPrice中比较优惠价格大小，函数minPrice，输出最小的价格以及优惠类型的object；
+  /*输入：
+    promotionPrice
+    itemArr
+  输出:
+    discountedPrice: {
+      price: number,
+      promotionType: string,
+      saveMoney: number,
+  }
+  */
 //[done]按格式指定清单，输入arguments包括菜品array，minPrice中得到的价格obejct，输出字符串；
-//[done]打印清单
+  /*输入：
+    itemArr
+    discountedPrice
+  输出：
+    receipt: string
+  */
+//[to-do]打印清单测试，写一个测试程序
